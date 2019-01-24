d9a253a004a2114b85d7eb97d45fa55eb9f4f896
BUMBALORD <davidhallinan@gmail.com>
2019-01-23 20:35:23 -0800

    Updates

---
+++ precache-manifest.4b4528a222fbe80ce13709f3bf8e1ea7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2688e96275edf3b53e71",
+    "url": "/testers/static/css/main.866babf2.chunk.css"
+  },
+  {
+    "revision": "2688e96275edf3b53e71",
+    "url": "/testers/static/js/main.2688e962.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/testers/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "f63e7f74d7781e5fde74",
+    "url": "/testers/static/js/runtime~main.f63e7f74.js"
+  },
+  {
+    "revision": "900379c3a729b68530af8caf0254dbad",
+    "url": "/testers/index.html"
+  }
+];
